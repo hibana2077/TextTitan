16b11fc3c9b05f05e243537971fe3678729c68e4
hibana2077 <hibana2077@gmail.com>
2024-08-28 17:59:32 +0800

    chore: Update tailwind.config.ts to add 'dodger-blue' color palette

--- src/web/tailwind.config.ts
+++ src/web/tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'dodger-blue': {
+          '50': '#f0f8fe',
+          '100': '#deeefb',
+          '200': '#c4e3f9',
+          '300': '#9bd1f5',
+          '400': '#6cb8ee',
+          '500': '#3d94e6',
+          '600': '#3480dc',
+          '700': '#2b6aca',
+          '800': '#2957a4',
+          '900': '#264a82',
+          '950': '#1c2f4f',
+        },
+      },
     },
   },
   plugins: [],
